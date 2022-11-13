bb6fb8bfdab689cb7c74873a726c2104609f25c1
jinchuanixong <117559567+jinchuanixong@users.noreply.github.com>
2022-11-13 16:31:25 +0800

    Add files via upload

--- UnitTest1.cpp
+++ UnitTest1.cpp
@@ -24,3 +24,43 @@ namespace UnitTest1
 		}
 	};
 }
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int maxSum = 0;
+			int nowSum = 0;
+			int n = 6;
+			int num[] = {-6，8，12，26，-15，23};
+			for (int i = 0;i < n;i++) {
+				nowSum += num[i];
+				maxSum=now(maxSum, &nowSum, n, num[i]);
+			}
+			Assert::AreEqual(20,maxSum);
+		}
+	};
+}
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int maxSum = 0;
+			int nowSum = 0;
+			int n = 6;
+			int num[] = {19，36，17，-14，52，1，8};
+			for (int i = 0;i < n;i++) {
+				nowSum += num[i];
+				maxSum=now(maxSum, &nowSum, n, num[i]);
+			}
+			Assert::AreEqual(20,maxSum);
+		}
+	};
+}
