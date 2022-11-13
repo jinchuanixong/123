3f09bab6254a9a5381dea8e0a595fe3112e79363
jinchuanixong <117559567+jinchuanixong@users.noreply.github.com>
2022-11-13 15:44:46 +0800

    Add files via upload

---
+++ UnitTest1.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+      
+#include "CppUnitTest.h"
+#include "../ConsoleApplication1/test.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int maxSum = 0;
+			int nowSum = 0;
+			int n = 6;
+			int num[] = {1，6，-9，16，12，-13};
+			for (int i = 0;i < n;i++) {
+				nowSum += num[i];
+				maxSum=now(maxSum, &nowSum, n, num[i]);
+			}
+			Assert::AreEqual(20,maxSum);
+		}
+	};
+}
